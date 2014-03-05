b495132befa7a67ac1948faeace3122c9dedbe11
bogoroh <mtaatgen@live.com>
2014-03-04 22:41:27 -0500

    Finished the story
    Finished the story

--- Lab1/main.py
+++ Lab1/main.py
@@ -27,7 +27,7 @@ foodIndex = random.randrange(0,5)#Find out what game we are playing
 foodString = food[foodIndex] #Hold the string for the casinoGames
 
 #Checking which nightclub you go to 
-nightclubs = {"Daytona":"Club XL","Orlando":"Vain Nightclub","Tampa":"XS Nightclub","Miami Beach":"Mansion"}
+nightclubs = dict("Daytona":"Club XL","Orlando":"Vain Nightclub","Tampa":"XS Nightclub","Miami Beach":"Mansion")
 nightclub = nightclubs["Tampa"] #This will call the value
 
 
@@ -48,6 +48,12 @@ def place():
     	place = "your place"
     	return place
 placeF = place()
-message = '''{opening} {name}. Do you remember what happend last night? You woke up at {woke}AM. Your friend {friendname} called you to ask if you wanted to go the {nightclub}. So you guys went. Since you didn't have any cash you had to go to the atm and pull out some cash. You got your receipt back and now only have {bankF}$.You entered the club and started dancing with this cute girl named {gname}.After spending {spent} at the club it was getting late you and her decided to buy something to eat before heading home and you spent {info[float]}. You both bought a {foodString} and decided to head to {placeF}. After {days} days you decided to get a tattoo with her name on it.'''
+
+tcolor = ""
+lights = ["Red","Yellow","Green"]
+for light in lights:
+	tcolor = tcolor + light + ", "
+
+message = '''{opening} {name}. Do you remember what happend last night? You woke up at {woke}AM. Your friend {friendname} called you to ask if you wanted to go the {nightclub}. The traffic light hit {tcolor}so you guys went. Since you didn't have any cash you had to go to the atm and pull out some cash. You got your receipt back and now only have {bankF}$. You entered the club and started dancing with this cute girl named {gname}. After spending {spent} at the club it was getting late you and her decided to buy something to eat before heading home. and you spent ${spent}. You both bought a {foodString} and decided to head to {placeF}. After {days} days you decided to get a tattoo with her name on it. '''
 messageFormatted = message.format(**locals())
 print messageFormatted
