4f40b93b75a49f5bad73f79453637beb41912e48
bogoroh <mtaatgen@live.com>
2014-03-11 22:01:46 -0400

    Added calcArea function
    Messed up and starting making it in googoleappengine

--- Quizes/Quiz1/main.py
+++ Quizes/Quiz1/main.py
@@ -2,17 +2,7 @@
 # 03-11-14
 # DPW2
 # Quiz 1
-
-import webapp2
-
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-		print calcArea(20,20)
-		countDown(20)
-
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+print calcArea(40,20)
 
 def calcArea (w,h):
 	area = str(w * h) #Stringify the area
@@ -23,16 +13,3 @@ def calcArea (w,h):
 	else: # If they are not identical
 		areaMessage = "The area of your rectangle is " + area + " square feet."
 		return area_message
-
-def countDown (b):
-	bottles = int(b)
-	for i in range(bottles, 0, -1): #bottles is the amount of beer bottles on the wall you want a range from the amount they have till 0 and subtracting 1 everytime
-		if i> 1: #still grabbing beer
-			message = str(i) + " Bottles of Beer on the Wall, " + str(i) +  " Bottles of Beer.. take one down and pass it around. Now you have " + str(i-1) + " bottles of beer on the wall!"
-			print message
-			print "----------------------------------------------"
-		else: #if you are grabbing your last beer
-			print "1 Bottle of beer on the wall, 1 Bottle of beer." +  "Bottles of Beer.. take one down and pass it around. Now you are out of beer and gotta call the cab"
-			print "ARGHHH, I'mz a pirate that needs more beer"		
-			
-			
