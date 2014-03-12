25fb279f370c3bc2bf4b9637fb15fe5ce78b5315
bogoroh <mtaatgen@live.com>
2014-03-11 22:18:07 -0400

    Got the first function working with a class

--- Quizes/Quiz1/main.py
+++ Quizes/Quiz1/main.py
@@ -2,14 +2,18 @@
 # 03-11-14
 # DPW2
 # Quiz 1
-print calcArea(40,20)
 
-def calcArea (w,h):
-	area = str(w * h) #Stringify the area
 
-	if (w == h): #check if the are identical
-		area_message = "The area of your square is " + area + " square feet."
-		return area_message
-	else: # If they are not identical
-		areaMessage = "The area of your rectangle is " + area + " square feet."
-		return area_message
+class CalcArea():
+	def calcArea (w,h):
+		area = str(w * h) #Stringify the area
+
+		if (w == h): #check if the are identical
+			print "The area of your square is " + area + " square feet."
+		else: # If they are not identical
+			print "The area of your rectangle is " + area + " square feet."
+			
+	area = calcArea(40,20)
+
+
+
