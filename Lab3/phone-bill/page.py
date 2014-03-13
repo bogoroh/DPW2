98ff2c2f4bc4988317acc9be74da0da37c8d242c
bogoroh <mtaatgen@live.com>
2014-03-13 10:10:20 -0400

    Added the template on page.py

--- Lab3/phone-bill/page.py
+++ Lab3/phone-bill/page.py
@@ -1,3 +1,30 @@
 class Page():
 	def __init__(self):
-		
+		self.__header = '''
+<!DOCTYPE>
+<html>
+<head>
+<title>Phone Calculator</title>
+<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+'''
+        
+		self.__form = '''
+
+'''
+        
+		self.__footer = '''
+
+'''
+	@property
+    def header(self):
+        return self.__header
+
+    @property
+    def form(self):
+        return self.__form
+
+    @property
+    def footer(self):
+        return self.__footer
