fcf9b1401d42942497f1ec644135efae3ce70197
bogoroh <mtaatgen@live.com>
2014-03-13 22:03:01 -0400

    added the super class, some attributes and methods

--- Quizes/Quiz2/main.py
+++ Quizes/Quiz2/main.py
@@ -1,3 +1,30 @@
 class videos(object):
+	def __init__(self):
+		length():
+			self.__time = 86
+			self.__rated = "R"
+			self.rating = 4.5
+			
+		main_actor(a,b):
 
-class tvseries(videos)
+
+
+	@property
+	def time(self):
+	    return self.__time
+	@time.setter
+	def time(self, value):
+	    self.__time = value
+		
+
+class tvseries(videos):
+	def __init__(self):
+		super(tvseries, self).__init__()
+	amount_of_seasons():
+
+
+	@foo.setter
+	def foo(self, value):
+	    self._foo = value
+	
+
