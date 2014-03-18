afaae0518270a83a2d5b93117c544940fbacb712
bogoroh <mtaatgen@live.com>
2014-03-18 16:34:31 -0400

    Added some more css

--- fox/page.py
+++ fox/page.py
@@ -6,53 +6,55 @@ class Page():
     <head>
             <title>Mike Taatgen's Fox</title>
             <link rel='stylesheet' href='css/main.css' />
+            <link href='http://fonts.googleapis.com/css?family=Averia+Sans+Libre' rel='stylesheet' type='text/css'>
+            <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
 </head>
     <body>
-        <div id="wrapper">'''
+    '''
         self.__nav='''
+        <h1> Animals </h1>
             <nav id="buttons">
+				
                 <a href='/?animal=0'><span>Red Panda</span></a>
                 <a href='/?animal=1'><span>Great Auk</span></a>
                 <a href='/?animal=2'><span>Leopard Cat</span></a>
             </nav>'''
         self.__content='''
-            <h1>I am called {obj.name}</h1>
-                <div>
+         	<div id="wrapper">
+            <h2>I am called {obj.name}</h2>
+                <div class="col3">
                     <h3>My phylum is:</h3>
                     <p>{obj.phylum}</p>
-                </div>
-                <div>
+
                     <h3>My class is:</h3>
                     <p>{obj.cclass}</p>
-                </div>
-                <div>
+              
                     <h3>My order is:</h3>
                     <p>{obj.order}</p>
-                </div>
-                <div>
+                
                     <h3>My family is:</h3>
                     <p>{obj.family}</p>
-                </div>
-                <div>
+               
                     <h3>My genus is:</h3>
                     <p>{obj.genus}</p>
+             	</div>
+                <div class="image">
+                	<h3>This is me:</h3>
+                	<img src='{obj.url}' width="550" height="400"/>
                 </div>
-                <div>
-                	<img src='{obj.url}'/>
-                </div>
-                <div>
+                <div class="info">
                     <h3>My lifespan is:</h3>
                     <p>{obj.alifespan}</p>
                 </div>
-                <div>
+                <div class="info">
                     <h3>My habitat is:</h3>
                     <p>{obj.habitat}</p>
                 </div>
-                <div>
+                <div class="info">
                     <h3>You can find me:</h3>
                     <p>{obj.geolocation}</p>
                 </div>
-                <div>
+                <div class="info">
                     <h3>I say:</h3>
                     <p>{obj.sound}</p>
                 </div>
