a319d2058abd9dea35e83ebc4775e3614d2f7536
bogoroh <mtaatgen@live.com>
2014-03-18 13:38:34 -0400

    Added the leopard class

--- fox/main.py
+++ fox/main.py
@@ -10,52 +10,52 @@ class MainHandler(webapp2.RequestHandler):
         page = Page()
 
 		# redPanda class that gets all the attributes from the AbstractAnimals class
-		redPanda = Sound()
-		redPanda.name = "Red Panda"
-		redPanda.phylum = "Chordata"
-		redPanda.cclass = "Mammalia"
-		redPanda.order = "Carnivora"
-		redPanda.family = "Ailuridae"
-		redPanda.genus = "Ailurus"
-		redPanda.url = "http://animaldiversity.ummz.umich.edu/accounts/Ailurus_fulgens/pictures/collections/contributors/Kjeldsen_Per/Red_Panda7208/"
-		redPanda.alifespan = "The maximum lifespan of the red panda is 14 years, but the average is eight to ten."
-		redPanda.habitat = "Red pandas live in temperate climates in deciduous and coniferous forests. There is usually an understory of bamboo and hollow trees. The average temperature is 10 to 25 degrees Celsius, and the average annual rainfall is 350 centimeters."
-		redPanda.geolocation = "Red pandas are found throughout the Himalayan mountains between 2,200 and 4,800 meters in elevation in northern Burma, Nepal, Sikkim region of India, and the districts of western Sichuan and Yunnan in China. Their geographic range is bounded in the north by the Namlung Valley in the Mugo District and the Lake Rara region of northern Nepal, in the south by the Liakiang Range of western Yunnan, and the northern and eastern boundary is the upper Min Valley of western Sichuan."
-		redPanda.sound = "Wieuw Wieuw"
-		redPanda.update()
+		red_panda = Sound()
+		red_panda.name = "Red Panda"
+		red_panda.phylum = "Chordata"
+		red_panda.cclass = "Mammalia"
+		red_panda.order = "Carnivora"
+		red_panda.family = "Ailuridae"
+		red_panda.genus = "Ailurus"
+		red_panda.url = "http://animaldiversity.ummz.umich.edu/collections/contributors/Kjeldsen_Per/Red_Panda7208/large.jpg"
+		red_panda.alifespan = "The maximum lifespan of the red panda is 14 years, but the average is eight to ten."
+		red_panda.habitat = "Red pandas live in temperate climates in deciduous and coniferous forests. There is usually an understory of bamboo and hollow trees. The average temperature is 10 to 25 degrees Celsius, and the average annual rainfall is 350 centimeters."
+		red_panda.geolocation = "Red pandas are found throughout the Himalayan mountains between 2,200 and 4,800 meters in elevation in northern Burma, Nepal, Sikkim region of India, and the districts of western Sichuan and Yunnan in China. Their geographic range is bounded in the north by the Namlung Valley in the Mugo District and the Lake Rara region of northern Nepal, in the south by the Liakiang Range of western Yunnan, and the northern and eastern boundary is the upper Min Valley of western Sichuan."
+		red_panda.sound = "Wieuw Wieuw"
+		red_panda.update()
 
-		# LeopardShark class that gets all the attributes from the AbstractAnimals class
-		leopardShark = Sound();
-		leopardShark.name = "Leopard Shark"
-		leopardShark.phylum = "Chordata"
-		leopardShark.cclass = "Chondrichthyes"
-		leopardShark.order = "Carcharhiniformes"
-		leopardShark.family = "Carcharhinidae"
-		leopardShark.genus = "Galeocerdo"
-		leopardShark.url = "http://www.elasmodiver.com/Sharkive%20images/Leopard_Shark224.jpg"
-		leopardShark.alifespan = "The average lifespan of tiger sharks in the wild is 27 years, though some may live to 50 years of age. "
-		leopardShark.habitat = "Tiger sharks are a saltwater species. Although they prefer the sea grass ecosystems of the costal areas, they occasionally inhabit other areas due to prey availability. Tiger sharks spend approximately 36&#37; of their time in shallow coastlne habitats (Heithaus et al., 2002), generally at depths of 2.5 to 145 m. This species, however, has been documented several kilometers from the shallow areas and at depths up to 350 m. Females are observed in shallow areas more often than males. Tiger sharks have also been documented in river estuaries and harbors"
-		leopardShark.geolocation = "Tiger sharks are found in many subtropical and tropical waters, primarily from 45&deg;N to 32&deg;S. Tiger sharks have been sighted from the eastern coast of North America to the eastern coast of Brazil. This includes the coasts of southern North America, Mexico, and Latin America along the Gulf of Mexico. Tiger sharks also populate the coasts of China, India, Africa, Japan, and many islands of the Pacific Ocean."
-		leopardShark.sound = "Wrah Wrah"
-		leopardShark.update()
+		# great_auk class that gets all the attributes from the AbstractAnimals class
+		great_auk = Sound();
+		great_auk.name = "Great Auk"
+		great_auk.phylum = "Chordata"
+		great_auk.cclass = "Aves"
+		great_auk.order = "Charadriiformes"
+		great_auk.family = "Alcidae"
+		great_auk.genus = "Pinguinus"
+		great_auk.url = "http://animaldiversity.ummz.umich.edu/collections/contributors/grzimek_birds/Alcidae/pinguinus_impennis/medium.jpg"
+		great_auk.alifespan = "As in other large seabirds, annual survival is thought to have been relatively high. Estimated lifespan was from 20 to 25 years."
+		great_auk.habitat = "In recorded history, great auks were found breeding only on isolated, rocky islands in the North Atlantic. They foraged in the open ocean when not breeding."
+		great_auk.geolocation = "The last great auk was seen in 1852 off the Newfoundland Banks. They were extinct in the western Atlantic before 1800 and persisted a bit longer in some parts of the eastern Atlantic and off the coast of Iceland. They lived and bred on scattered, offshore islands in the northern Atlantic in recorded history, although it is possible they once occurred on continental coastlines but were extirpated from those areas earlier. They were found from Canada, Greenland, and Iceland to the British Isles and Scandinavia. Colonies in the western Atlantic may have been less numerous than in the eastern Atlantic. Archeological records indicate they once occurred as far south as southern Spain and New England in the United States, Pleistocene records indicate great auks occurred as far south as Italy and other parts of the Mediterranean. Great auks became extinct before any natural history studies were conducted, so little is known about their lives except for a few studies of lone, captive birds and the casual records of mariners."
+		great_auk.sound = "Wrah Wrah"
+		great_auk.update()
 
-		# Blue Marlin class that gets all the attributes from the AbstractAnimals class
-		blueMarlin = Sound()
-		blueMarlin.name = "Blue Marlin"
-		blueMarlin.phylum = "Chordata"
-		blueMarlin.cclass = "Actinopterygii"
-		blueMarlin.order = "Perciformes"
-		blueMarlin.family = "Istiophoridae"
-		blueMarlin.genus = "Makaira"
-		blueMarlin.url = "http://animaldiversity.ummz.umich.edu/collections/contributors/Grzimek_fish/Scombroidei/Makaira_nigricans/button.jpg"
-		blueMarlin.alifespan = "The maximum lifespan of females is estimated to be at least 27 years, while males are estimated to live a maximum of 18 years."
-		blueMarlin.habitat = "Makaira nigricans is an epipelagic and oceanic species. It is the most oceanic of all istiophorids, usually remaining far from land except where the continental shelf is narrow. It can be found in waters with surface temperatures of 22-31C, but it prefers the warm mixed layer above the thermocline, and spends the majority of its time in the uniformly warm near-surface waters from 25-27C. It shows preference for blue waters, at least in the northern Gulf of Mexico."
-		blueMarlin.geolocation = "<p>Makaira nigricans is distributed mainly in the tropical and temperate waters of the Atlantic, Pacific, and Indian Oceans. It is the most tropical of all billfishes.</p> <p>In the Atlantic Ocean, its range extends to around 40-45N in the North Atlantic and to 40S in the western Atlantic, 30S in the central South Atlantic and 35S in the eastern south Atlantic, but is absent from the Mediterranean Sea. In the Pacific, its range extends to about 45N in the western North Pacific, 35N in the eastern North Pacific, 35S in the western South Pacific, and 25S in the eastern South Pacific. </p> <p> In the Indian Ocean, its range extends to 45S in the southwestern Indian Ocean and 35S in the southeastern Indian Ocean. Larvae are found extensively in the tropical and subtropical waters of the western and central Pacific Ocean, south of Maldives Islands, around the Mascalene Islands, and off the south coasts of Java and Sumatra in the Indian Ocean. In the western central Atlantic, larvae are found off Georgia, North Carolina, Florida, Jamaica, Bahamas, Arecibo, and also off Brazil in the southwest Atlantic. </p>"	
-		blueMarlin.sound = "Whish Whish"
-		blueMarlin.update()
+		# Leopard Cat class that gets all the attributes from the AbstractAnimals class
+		leopard_cat = Sound()
+		leopard_cat.name = "Blue Marlin"
+		leopard_cat.phylum = "Chordata"
+		leopard_cat.cclass = "Mammalia"
+		leopard_cat.order = "Carnivora"
+		leopard_cat.family = "Felidae"
+		leopard_cat.genus = "Prionailurus"
+		leopard_cat.url = "http://animaldiversity.ummz.umich.edu/collections/contributors/tanya_dewey/leopardcat/large.jpg"
+		leopard_cat.alifespan = "In the wild, leopard cats have an average lifespan of about 4 years, and have been known to live up to 20 years in captivity. The lifespan of captive individuals varies greatly as individuals may die from the stress of transport. When leopard cats are released into non-native environments by breeders, they usually die not long after."
+		leopard_cat.habitat = "Prionailurus bengalensis is found in tropical and temperate forests, coniferous forests, shrub land habitat, and grasslands. Its distribution is limited to areas with less than 10 cm of snow annually, and it is not found in steppe or arid climates. Prionailurus bengalensis has a fairly diverse diet and is able to find food in most habitats. It seems relatively impervious to human disturbance as populations in secondary growth and disturbed areas are stable and it is often found near agricultural fields and rural settlements. Prionailurus bengalensis is an exceptional swimmer, possibly explaining its distribution on islands, and is intolerant of temperatures above 35 C, possibly explaining its absence from central India. It is capable of living at higher elevations (i.e., 3000 m) with minimal snow fall."
+		leopard_cat.geolocation = "Prionailurus bengalensis is one of the most widespread carnivore species in Asia, and can be found throughout most of southern Asia. Prionailurus bengalensis occupies eastern Afghanistan and northern Pakistan, northern and coastal India, Myanmar, Laos, Thailand, Indonesia, Malaysia, Vietnam, Taiwan, Sumatra, Java, Bali, Borneo, Nepal, Korea, Cambodia, parts of the Philippines, and Eastern China. Prionailurus bengalensis has been divided into a number of subspecies over its range that differ in coloration, pelage, body length, and reproductive cycles."	
+		leopard_cat.sound = "Whish Whish"
+		leopard_cat	.update()
 		
 		#Array with all the animals in it
-		animals = [blackDolphins,leopardShark,blueMarlin]
+		animals = [red_panda,great_auk,leopard_cat]
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
