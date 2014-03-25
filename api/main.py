8d9d4ca728307e7476cab5db0c85d735d1c6b51d
bogoroh <mtaatgen@live.com>
2014-03-25 11:56:46 -0400

    A - content
    Added the content to get the information looping from the XML that has
    been getting back

--- api/main.py
+++ api/main.py
@@ -28,6 +28,28 @@ class MainHandler(webapp2.RequestHandler):
 			#Stores user and pass to library's authentication helper
 			auth_manager = urllib2.HTTPBasicAuthHandler(password_manager)
 
+			#creates "openeer object for fetching page info"
+			self.__opener = urllib2.build_opener(auth_manager)
+			urllib2.install_opener(self.__opener)
+			handler = urllib2.urlopen(self.__req)
+			openerURL = urllib2.build_opener() #magic to load request - creates framework to get url
+			result = openerURL.open(self.__req) # gets url and puts result in "result"
+			xmldoc = minidom.parse(result) #parse through string to get XML object 
+			content = '<br/>' 
+			ARit = xmldoc.getElementsByTagName('RitNummer')
+			AVertrekTijd = xmldoc.getElementsByTagName('VertrekTijd') # Saved the departure time for that station into an array
+			AEind = xmldoc.getElementsByTagName('EindBestemming') # Saves all the final destination into an array  for that station
+			ATrein = xmldoc.getElementsByTagName('TreinSoort') # Saves all the Traintypes in an array for that station
+			AVertrek = xmldoc.getElementsByTagName('VertrekSpoor') #Saves all the Departure railways for that station into an array 
+			self.response.write(AVertrek)
+			for l,m,n,o,p in zip(ARit, AVertrekTijd,AEind,ATrein,AVertrek): # Makes it possible to loop throught multiple arrays
+				content += 'Trainnumber: ' + l.firstChild.nodeValue 
+				content += 'Departure Time: ' + m.firstChild.nodeValue
+				content += 'Final Destination: ' + n.firstChild.nodeValue
+				content += 'Traintype: ' + o.firstChild.nodeValue
+				content += 'Departure Railway: '  + p.firstChild.nodeValue
+				content += "<br/>"
+			self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
