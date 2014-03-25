07e5ebd1a4332b59c8cc4b764e48ab4462685a2d
bogoroh <mtaatgen@live.com>
2014-03-25 11:55:20 -0400

    A - Authentication
    Added authentication for the trainstation

--- api/main.py
+++ api/main.py
@@ -6,7 +6,50 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+		page = Page()
+		form_settings = [{"name":"station","type":"text","label":"Enter the abrevation of the station "},{"name":"submit","type":"submit","label":"Get departure times"}]
+		form = Form(form_settings)
+		form.update()
+		self.response.write(form.header + form.getForm + form.close)
+		if self.request.GET:
+			print "I ran"
+			self.response.write(self.request.GET['station']) 
+			self.__station = self.request.GET['station']
+			self.__user = 'mtaatgen@live.com' #Authenticator username
+			self.__pass = "dv8kZ24iGNZwFiHdZrClS_vaNCKTz1rY5jO9GckIj-fgVEqcgpcyLA"
+
+			self.__url = "http://webservices.ns.nl/ns-api-avt?station=" #URL from the API
+			self.__req = urllib2.Request(self.__url + self.__station)
+			
+			#Creates authentication helper
+			password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+			#Binds username and pass to url
+			password_manager.add_password(None,self.__url+self.__station,self.__user,self.__pass)
+			#Stores user and pass to library's authentication helper
+			auth_manager = urllib2.HTTPBasicAuthHandler(password_manager)
+
+			#creates "openeer object for fetching page info"
+			self.__opener = urllib2.build_opener(auth_manager)
+			urllib2.install_opener(self.__opener)
+			handler = urllib2.urlopen(self.__req)
+			openerURL = urllib2.build_opener() #magic to load request - creates framework to get url
+			result = openerURL.open(self.__req) # gets url and puts result in "result"
+			xmldoc = minidom.parse(result) #parse through string to get XML object 
+			content = '<br/>' 
+			ARit = xmldoc.getElementsByTagName('RitNummer')
+			AVertrekTijd = xmldoc.getElementsByTagName('VertrekTijd') # Saved the departure time for that station into an array
+			AEind = xmldoc.getElementsByTagName('EindBestemming') # Saves all the final destination into an array  for that station
+			ATrein = xmldoc.getElementsByTagName('TreinSoort') # Saves all the Traintypes in an array for that station
+			AVertrek = xmldoc.getElementsByTagName('VertrekSpoor') #Saves all the Departure railways for that station into an array 
+			self.response.write(AVertrek)
+			for l,m,n,o,p in zip(ARit, AVertrekTijd,AEind,ATrein,AVertrek): # Makes it possible to loop throught multiple arrays
+				content += 'Trainnumber: ' + l.firstChild.nodeValue 
+				content += 'Departure Time: ' + m.firstChild.nodeValue
+				content += 'Final Destination: ' + n.firstChild.nodeValue
+				content += 'Traintype: ' + o.firstChild.nodeValue
+				content += 'Departure Railway: '  + p.firstChild.nodeValue
+				content += "<br/>"
+			self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
--- api/page.py
+++ api/page.py
@@ -1,6 +1,6 @@
 class Page(object):
 	def __init__(self):
-			self.title = "NS - Traininfo"
+			self.title = "NS - Traininf"
 			self.css = '<link rel="stylesheet" href="css/main.css" type="text/css" />'
 			self._header ='''<!DOCTYPE>
 <html>
@@ -23,4 +23,31 @@ class Page(object):
 		return self.__closer
 
 	def update(self):
-		self._header = self._header.format(**locals())		
+		self._header = self._header.format(**locals())
+
+class Form(Page):
+	# name #Type #labels
+	#{"name":"zip","type":"text","label":"Enter your zipcode"}
+	def __init__(self,obj):
+		super(Form,self).__init__()
+		self.method = "GET"
+		self.action = ""
+		self.__formOpen = '''
+<form action="{self.action}" method="{self.method}">'''
+
+		self.__inputs = ''
+		for el in obj:
+			print el['name']
+			if el['type'] == "text":
+				self.__inputs += '<input type="' + el['type']  + '" placeholder="' + el['label'] +'" name="' + el['name'] + '"></input>'
+			else:
+				self.__inputs += '<input type="' + el['type']  + '" value="' + el['label']  +'" name="' + el['name'] + '"></input>'	
+		self.__formClose = "</form>"
+
+	@property
+	def getForm(self):
+	    return self.__formOpen + self.__inputs + self.__formClose
+	
+	def update(self):
+		self._header = self._header.format(**locals())
+		self.__formOpen = self.__formOpen.format(**locals())
