4714152e8b1650710f782984a34a1948d8416de4
bogoroh <mtaatgen@live.com>
2014-03-25 19:01:03 -0400

    Revert df2d4c1..a8a7768
    This rolls back to commit df2d4c1c4ada36c2ebdf07ec0e3a4a47f72f615f.

--- api/main.py
+++ api/main.py
@@ -16,32 +16,48 @@ class MainHandler(webapp2.RequestHandler):
 		if self.request.GET:
 			print "I ran"
 			#self.response.write(self.request.GET['station']) 
-			rstation = self.request.GET['station'].lower()
-			if rstation == "alkmaar":
+			self.rstation = self.request.GET['station'].lower()
+			if self.rstation == "alkmaar":
 				self.__sresponse = "amr"
-			elif rstation == "apeldoorn":
+			elif self.rstation == "apeldoorn":
 				self.__sresponse = "apd"
-			elif rstation == "breda":
+			elif self.rstation == "breda":
 				self.__sresponse = "bd"
-			elif rstation == "delft": 
+			elif self.rstation == "delft": 
 				self.__sresponse = "dt"
-			elif rstation == "emmen": 
+			elif self.rstation == "emmen": 
 				self.__sresponse = "emn"
-			elif rstation == "gouda": 
+			elif self.rstation == "gouda": 
 				self.__sresponse = "gd"
-			elif rstation == "haarlem": 
+			elif self.rstation == "haarlem": 
 				self.__sresponse = "hlm"
 			else:
 				self.__sresponse =  self.request.GET['station']
 			self.__station = self.__sresponse
+			self.__user = 'mtaatgen@live.com' #Authenticator username
+			self.__pass = "dv8kZ24iGNZwFiHdZrClS_vaNCKTz1rY5jO9GckIj-fgVEqcgpcyLA"
+
+			self.__url = "http://webservices.ns.nl/ns-api-avt?station=" #URL from the API
+			self.__req = urllib2.Request(self.__url + self.__station)
 			
-			
-			
+			#Creates authentication helper
+			password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
+			#Binds username and pass to url
+			password_manager.add_password(None,self.__url+self.__station,self.__user,self.__pass)
+			#Stores user and pass to library's authentication helper
+			auth_manager = urllib2.HTTPBasicAuthHandler(password_manager)
+
+			#creates "openeer object for fetching page info"
+			self.__opener = urllib2.build_opener(auth_manager)
+			urllib2.install_opener(self.__opener)
+			handler = urllib2.urlopen(self.__req)
+			openerURL = urllib2.build_opener() #magic to load request - creates framework to get url
+			result = openerURL.open(self.__req) # gets url and puts result in "result"
+			xmldoc = minidom.parse(result) #parse through string to get XML object 
 			content = '''
 	<ul>'''
 			ARit = xmldoc.getElementsByTagName('RitNummer')
 			AVertrekTijd = xmldoc.getElementsByTagName('VertrekTijd') # Saved the departure time for that station into an array
-			vyear = AVertrekTijd[:4]
 			AEind = xmldoc.getElementsByTagName('EindBestemming') # Saves all the final destination into an array  for that station
 			ATrein = xmldoc.getElementsByTagName('TreinSoort') # Saves all the Traintypes in an array for that station
 			AVertrek = xmldoc.getElementsByTagName('VertrekSpoor') #Saves all the Departure railways for that station into an array 
@@ -51,12 +67,11 @@ class MainHandler(webapp2.RequestHandler):
 				for l,m,n,o,p in zip(ARit, AVertrekTijd,AEind,ATrein,AVertrek): # Makes it possible to loop throught multiple arrays
 					content += '''
 		<li>'''
-					content += 'Trainnumber: ' + l.firstChild.nodeValue
+					content += 'Trainnumber: ' + l.firstChild.nodeValue 
 					content += 'Departure Time: ' + m.firstChild.nodeValue
 					content += 'Final Destination: ' + n.firstChild.nodeValue
 					content += 'Traintype: ' + o.firstChild.nodeValue
 					content += 'Departure Railway: '  + p.firstChild.nodeValue
-					content += vyear
 					content += '</li>'
 				content += '''
 	</ul>'''
@@ -66,63 +81,7 @@ class MainHandler(webapp2.RequestHandler):
 				self.response.write(form.header + form.getForm + content + form.close)
 		else:
 			self.response.write(form.header + form.getForm + form.close)
-
-class ApiModel(object):
-	def __init__(self,rstation):
-		self.__user = 'mtaatgen@live.com' #Authenticator username
-		self.__pass = "dv8kZ24iGNZwFiHdZrClS_vaNCKTz1rY5jO9GckIj-fgVEqcgpcyLA"
-
-		self.__url = "http://webservices.ns.nl/ns-api-avt?station=" #URL from the API
-		self.__req = urllib2.Request(self.__url + self.__station)
-		
-		#Creates authentication helper
-		password_manager = urllib2.HTTPPasswordMgrWithDefaultRealm()
-		#Binds username and pass to url
-		password_manager.add_password(None,self.__url+self.__station,self.__user,self.__pass)
-		#Stores user and pass to library's authentication helper
-		auth_manager = urllib2.HTTPBasicAuthHandler(password_manager)
-
-		#creates "openeer object for fetching page info"
-		self.__opener = urllib2.build_opener(auth_manager)
-		urllib2.install_opener(self.__opener)
-		handler = urllib2.urlopen(self.__req)
-		openerURL = urllib2.build_opener() #magic to load request - creates framework to get urllib2
-		self.send()
-	def send(self):
-		result = openerURL.open(self.__req) # gets url and puts result in "result"
-		self.sort()
-	def sort(self):
-		xmldoc = minidom.parse(result) #parse through string to get XML object 
-		self.__do = ApiData()
-		ARit = xmldoc.getElementsByTagName('RitNummer')
-		AVertrekTijd = xmldoc.getElementsByTagName('VertrekTijd') # Saved the departure time for that station into an array
-		AEind = xmldoc.getElementsByTagName('EindBestemming') # Saves all the final destination into an array  for that station
-		ATrein = xmldoc.getElementsByTagName('TreinSoort') # Saves all the Traintypes in an array for that station
-		AVertrek = xmldoc.getElementsByTagName('VertrekSpoor') #Saves all the Departure railways for that station into an array 
-		#self.response.write(AVertrek)
-
-		if ARit:
-			for l,m,n,o,p in zip(ARit, AVertrekTijd,AEind,ATrein,AVertrek): # Makes it possible to loop throught multiple arrays
-				tempDict = dict() # creates a set of info
-				content += '''
-	<li>'''
-				tempDict['tn'] += 'Trainnumber: ' + l.firstChild.nodeValue 
-				content['dt'] += 'Departure Time: ' + m.firstChild.nodeValue
-				content['fd'] += 'Final Destination: ' + n.firstChild.nodeValue
-				content['tt'] += 'Traintype: ' + o.firstChild.nodeValue
-				content['dr'] += 'Departure Railway: '  + p.firstChild.nodeValue
-				content += '</li>'
-				self.__do.forecast.append(tempDict) # Pushes info into the arrays
-			content += '''
-</ul>'''
-
-class ApiData(object):
-	def __init__(self):
-
-
-class ApiView(object):
-	def __init__(self):
-
+			
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
